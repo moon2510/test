054e24a02398722490ea8e2e0db40b2679afe20e
Nguyet <nguyetminhthibui@gmail.com>
2021-03-01 12:19:37 +0700

    create default user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserSeeder::class);
-        $this->call(CategorySeeder::class);
-        $this->call(BookSeeder::class);
-        $this->call(ConfigSeeder::class);
+        //$this->call(UserSeeder::class);
+        //$this->call(CategorySeeder::class);
+        //$this->call(BookSeeder::class);
+        //$this->call(ConfigSeeder::class);
+        User::create([
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'gender' => 1,
+            'address' => 'sadasd',
+            'roles' => 1,
+            'password' => bcrypt('admin'), // password
+            'remember_token' => Str::random(10),
+            'phone' => 0000000000,
+            'firstname' => $faker->name,
+            'lastname' => $faker->name
+        ])
     }
 }
